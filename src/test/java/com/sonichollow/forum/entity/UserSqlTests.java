3b2f6fee43a00e9e5ac800470c6af6be077c0387
墨鳌 <83717535+MoAo-01@users.noreply.github.com>
2022-03-30 12:45:51 +0800

    @SpringBootTest;

--- src/test/java/com/sonichollow/forum/entity/UserSqlTests.java
+++ src/test/java/com/sonichollow/forum/entity/UserSqlTests.java
@@ -4,9 +4,12 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.sonichollow.forum.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+
+@SpringBootTest
 public class UserSqlTests {
     @Autowired
     private UserMapper userMapper;
